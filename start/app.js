812f9bab27e73e3ad21cebf27fd356ae70f7ffe4
Ludovic <dolu89@gmail.com>
2019-03-26 19:34:11 +0100

    clean up project

--- start/app.js
+++ start/app.js
@@ -12,7 +12,6 @@
 */
 const providers = [
   '@adonisjs/framework/providers/AppProvider',
-  '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
